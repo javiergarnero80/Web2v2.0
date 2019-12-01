55d69d1342c98db83bf837651c23acfbffea5cb3
javiergarnero80 <38335131+javiergarnero80@users.noreply.github.com>
2019-12-01 19:41:45 -0300

    Original Web 2

---
+++ SitioWebHotel/src/com/hotel/models/UsuariosModel.java
@@ -0,0 +1,13 @@
+package com.hotel.models;
+
+import com.hotel.beans.Usuario;
+
+public class UsuariosModel extends Conexion {
+
+	public UsuariosModel() {
+		// TODO Auto-generated constructor stub
+	}
+   
+  
+
+}
